ffcd3fe4221c0ee81464c17b0bef0b766855f973
unknown <huutam.ptnk@gmail.com>
2018-11-15 05:00:03 +0200

    sketched the static blinking point

--- lib/circle.dart
+++ lib/circle.dart
@@ -12,11 +12,19 @@ class Circle extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    // Draw the mid point
     Paint line = new Paint();
+    line.strokeCap = StrokeCap.round;
     line.color = color;
+    line.strokeWidth = 3.0;
     Offset center = new Offset(size.width / 2, size.height / 2);
     double radius = pointSize;
+
+    line.style = PaintingStyle.fill;
     canvas.drawCircle(center, radius, line);
+
+    line.style = PaintingStyle.stroke;
+    canvas.drawCircle(center, radius * 3, line);
   }
 
   @override
